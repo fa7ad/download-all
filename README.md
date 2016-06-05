69a8c910a45b128d6a4f4d251b6806ac2368e759
Fahad Hossain <oneloopone@gmail.com>
2016-06-05 08:42:33 +0600

    support all extensions, and other downloaders

--- README.md
+++ README.md
@@ -2,9 +2,16 @@
 ---
 A very rudimentary script that downloads all zip and rar files from a given page
 
+## Installation
+---
+```bash
+[sudo] npm i -g download-all-files
+```
+
 ## Usage:
 ---
 ```bash
-dlaf
+dlaf [link to page]
 ```
-And then enter the URL when asked for.
+And then enter the downloader options when asked.
+Used CURL by default, works with wget as well.
--- cli.js
+++ cli.js
@@ -1,67 +1,119 @@
 #!/usr/bin/env node
-
+'use strict';
 const axios = require('axios');
 const cheerio = require('cheerio');
 const inquirer = require('inquirer');
-const spawn = require('child_process').spawn;
-const resolve = require('url').resolve;
+const fs = require('fs');
+const path = require('path');
+const { resolve } = require('url');
+const { spawn } = require('child_process');
 
 
-function errorHandler(err) {
-    console.dir(err);
+let errorHandler = err => console.log(`Error ${err.status || ''}. ${err.statusText || ''}`);
+
+
+let crawl_links = (url, dir) =>
+  axios
+    .get(url)
+    .then(function(res) {
+      let $ = cheerio.load(res.data);
+      let links = [];
+      $('a').each(function() {
+        let link = $(this).attr('href');
+        if (link) {
+          let lin = resolve(url, link);
+          if (/\.\w{2,4}$/.test(lin)) {
+            links.push(lin);
+          }
+        }
+      });
+      if (links.length > 0) {
+        download_url(links, dir);
+      }
+    })
+    .catch(errorHandler)
+;
+
+
+let output_link = function(link, folder) {
+  let b_link = link.split('/');
+  let fname = b_link[b_link.length-1];
+  if (/&/.test(fname)) {
+    fname = fname.split('=').slice(-1)[0];
+  }
+  let file_name = path.join(path.resolve(folder), decodeURI(fname));
+  return file_name;
+};
+
+
+var download_url = function(urls, dir) {
+  inquirer
+    .prompt([
+      {
+        name: 'bin',
+        message: 'Downloader (use full path if possible)',
+        type: 'input',
+        default: 'curl'
+      },
+      {
+        name: 'args',
+        message: 'CLI argument flags (other than output, - to skip)',
+        type: 'input',
+        default: '-L'
+      },
+      {
+        name: 'out',
+        message: 'CLI output flags (comes right before output)',
+        type: 'input',
+        default: '-o'
+      }
+    ])
+    .then((res) => {
+      let arg = [];
+      if (res.args !== '-')
+        arg = res.args.split(' ');
+      urls.forEach(function(url) {
+        let file_name = output_link(url, dir);
+        spawn(res.bin, arg.concat(url, res.out, file_name));
+        console.log('Downloading', url, 'as', file_name);
+      });
+    });
+};
+
+
+let ask_link = function() {
+  let link = process.argv[2];
+  let questions = [];
+  questions.push;
+
+  if (!link) {
+    questions.push({
+      name: 'link',
+      message: 'Give link to the page you wanna download from.',
+      type: 'input'
+    });
+  }
+
+  questions.push({
+    name: 'dir',
+    message: 'Output directory',
+    type: 'input',
+    default: path.resolve('.')
+  });
+
+  inquirer
+    .prompt(questions)
+    .then(ans =>
+      fs.stat(ans.dir, function(err, stat) {
+        if (!stat) {
+          fs.mkdirSync(ans.dir);
+        }
+        return crawl_links(link || ans.link, ans.dir);
+      })
+    );
+};
+
+
+if (require.main === module) {
+  ask_link();
 }
-
-
-function crawl_links(url) {
-    const crawler = axios.create({url: url});
-    crawler
-        .get(url)
-        .then(function(res) {
-            let $ = cheerio.load(res.data);
-            $('a').each(function() {
-                let link = $(this).attr('href');
-                if (link){
-                    let lin = resolve(url, link);
-                    if(/\.(zip|rar|iso|dat|exe|msi|7z)$/.test(lin))
-                        download_url(lin);
-                }
-            });
-        })
-        .catch(errorHandler);
-}
-
-
-function download_url(url) {
-    let b_link = url.split('/');
-    let fname = b_link[b_link.length-1];
-    if (/&/.test(fname))
-        fname = decodeURI(fname.split('=').slice(-1)[0]);
-    try {
-        spawn('wget', [url, '-O', fname]);
-        console.log('Downloading', url, 'as', fname);
-    } catch (e) {
-        console.log('Please install "wget"');
-    } finally {
-        spawn('curl', ['-sL', url, '-o', fname]);
-        console.log('Downloading', url, 'as', fname);
-    }
-}
-
-
-function ask_link() {
-    let question = {
-        name: 'link',
-        message: 'Give link to the page you wanna download from.',
-        type: 'input'
-    };
-
-    inquirer
-        .prompt([question])
-        .then(ans => {
-            crawl_links(ans.link);
-        });
-}
-
-
-if (require.main === module)
-    ask_link();
